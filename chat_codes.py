bc1a8bedd7ad838cc57c2b6d2d1726a2ac7967f3
ivaely <nikolov.ivaely@gmail.com>
2025-09-28 01:38:03 +0200

    Add files via upload

---
+++ chat_codes.py
@@ -0,0 +1,12 @@
+number_of_msgs = int(input())
+
+for i in range(number_of_msgs):
+    msg_code = int(input())
+    if msg_code == 88:
+        print("Hello")
+    elif msg_code == 86:
+        print("How are you?")
+    elif msg_code < 88:
+        print("GREAT!")
+    elif msg_code > 88:
+        print("Bye.")
